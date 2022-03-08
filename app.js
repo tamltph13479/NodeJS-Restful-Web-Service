04b8dd9d253eef65c312414a912556d15990aa45
tamltph13479 <thetam1002@gmail.com>
2022-03-09 00:09:44 +0700

    setup

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === "/product") {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<body>");
+        res.write("<h1>Product Page</h1>");
+        res.write("<body>");
+        res.write("<html>");
+        res.end();
+    } else if (req.url === "/api/products") {
+        const products = [
+            { id: 1, name: "Product A" },
+            { id: 2, name: "Product B" },
+
+        ];
+        res.end(JSON.stringify(products));
+    } else {
+        console.log("Chịu không biết");
+    }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log("Server is running port", PORT);
+});
